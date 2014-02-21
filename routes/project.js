73885e8cbf17088a257256589c6f1a707b32898a
miad92 <miadavis@stanford.edu>
2014-02-20 19:46:02 -0800

    third time's the charm

--- routes/project.js
+++ routes/project.js
@@ -2,18 +2,12 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-  models.project
+  models.Project
   .find({"_id": projectID})
   .sort('-date')
   .exec(afterQuery);
   // query for the specific project and
   // call the following callback
-
-  function afterQuery(err, projects) {
-    if(err) console.log(err);
-    res.json(projects[0]);
-  }
-}
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
